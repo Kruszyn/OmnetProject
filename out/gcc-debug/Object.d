fe1114cd9619d2c0b89bbca766c204b4a170094e
Kruszyn <kkorenkiewicz1@gmail.com>
2018-05-12 15:49:31 +0200

    Statystki czasu

--- out/gcc-debug/Object.d
+++
@@ -1,8 +0,0 @@
-out/gcc-debug//Object.o: Object.cc \
- /home/student/omnetpp-5.2.1/include/omnetpp.h mymessage_m.h Object.h
-
-/home/student/omnetpp-5.2.1/include/omnetpp.h:
-
-mymessage_m.h:
-
-Object.h:
--- out/gcc-debug/TestObject.d
+++
@@ -1,6 +0,0 @@
-out/gcc-debug//TestObject.o: TestObject.cc \
- /home/student/omnetpp-5.2.1/include/omnetpp.h mymessage_m.h
-
-/home/student/omnetpp-5.2.1/include/omnetpp.h:
-
-mymessage_m.h:
